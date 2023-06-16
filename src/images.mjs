05eae7f941f62b8646ff61cd6bb4ac7937773e4f
Carlos Flores <carlos@floresbenavides.com>
2023-06-15 21:13:53 -0600

    make images and favicon optinoals

--- src/images.mjs
+++ src/images.mjs
@@ -1,20 +1,31 @@
 import {
   copyFileSync as cf,
   readdirSync as rd,
-  mkdirSync as mk
+  mkdirSync as mk,
+  existsSync as ex
 } from 'fs'
 import { resolve } from 'path'
 
-export const execute = (publicDir, templateDir) => {
-  mk(resolve(publicDir, 'images'))
-
+const migrateImages = (templateDir, publicDir) => {
+  if (!ex(resolve(templateDir, 'images'))) return
   console.log('- Migrating images'.magenta)
   const images = rd(resolve(templateDir, 'images'))
   images.forEach(image => {
     cf(resolve(templateDir, 'images', image), resolve(publicDir, 'images', image))
   })
+}
+
+const migrateFavicons = (templateDir, publicDir) => {
+  if (!ex(resolve(templateDir, 'favicon'))) return
+  console.log('- Migrating favicons'.magenta)
   const favicon = rd(resolve(templateDir, 'favicon'))
   favicon.forEach(item => {
     cf(resolve(templateDir, 'favicon', item), resolve(publicDir, item))
   })
+}
+
+export const execute = (publicDir, templateDir) => {
+  mk(resolve(publicDir, 'images'))
+  migrateImages(templateDir, publicDir)
+  migrateFavicons(templateDir, publicDir)
 }
